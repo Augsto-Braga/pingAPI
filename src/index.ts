ada94952a9211d0895019f2f9747f2b97451010e
Augusto Braga <bragaaugusto361@gmail.com>
2022-06-28 20:26:30 -0300

    fix script dev

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express, { Express, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json);
 
 app.get("/ping", (req: Request, res: Response) => {
   return res.send("pong");
